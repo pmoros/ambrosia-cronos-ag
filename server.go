ef9db4df619e8628961f595e26f68e55f69ae5da
Santiago Mondragón Gómez <santiago.mondragon@mercadolibre.com.co>
2022-11-14 15:50:57 -0500

    allow all origins

--- server.go
+++ server.go
@@ -26,7 +26,6 @@ func main() {
 	// Add CORS middleware around every request
 	// See https://github.com/rs/cors for full option listing
 	router.Use(cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000"},
 		AllowCredentials: true,
 		Debug:            true,
 	}).Handler)
